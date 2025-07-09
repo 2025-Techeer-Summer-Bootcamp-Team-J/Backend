9e936a654dded66b09f4d249d730a7be1161a118
jiyeon0323 <k.ji.noey@gmail.com>
2025-07-09 21:15:58 +0900

    [Feat]특정 사용자의 모든 진단 목록 조회api 구현 /#6 (#22)
    * feat/#6:특정 사용자의 모든 진단 목록 조회api 구현
    
    * feat/#6:특정 사용자의 모든 진단 목록 조회api 구현

--- api/router.py
+++ api/router.py
@@ -1,9 +1,11 @@
 from fastapi import APIRouter
 from api.endpoints import post
 from api.endpoints import skintype
+from api.endpoints import diagnosis
 
 api_router = APIRouter(
     prefix="/api"
 )
 api_router.include_router(post.router)
 api_router.include_router(skintype.router)
+api_router.include_router(diagnosis.router)
