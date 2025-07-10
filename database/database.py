754b3ffae3d86688580c9553d7dfd30af4355941
woomin <chlwoomin@naver.com>
2025-07-10 21:18:35 +0900

    민감 정보들 .env로 이동 완료

--- database/database.py
+++ database/database.py
@@ -1,14 +1,12 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
-
-# mysql db 연결
+from os import getenv
 # local 실행
 # SQLALCHEMY_DATABASE_URL = "mysql+pymysql://backend_user:backend_password@localhost:3306/db"
 # docker 실행
-SQLALCHEMY_DATABASE_URL = "mysql+pymysql://backend_user:backend_password@mysql:3306/db"
+SQLALCHEMY_DATABASE_URL = getenv("MYSQL_URL")
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
-
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
@@ -18,4 +16,3 @@ def get_db():
         yield db
     finally:
         db.close()
-
