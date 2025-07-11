fa40a8c08392ccfee8569e8f3a538f515df6d2cb
unknown <chlwoomin@naver.com>
2025-07-11 15:54:16 +0900

    dotenv 의존성 등 조금 수정

--- database/database.py
+++ database/database.py
@@ -1,6 +1,9 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from dotenv import load_dotenv
+import os
+load_dotenv()
 
 # mysql db 연결
 # docker 실행
