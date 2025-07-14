e91a97e814d735c5f881886e652a656cf8050a5e
unknown <chlwoomin@naver.com>
2025-07-14 16:41:21 +0900

    diseases 예외처리 수정

--- api/endpoints/diseases.py
+++ api/endpoints/diseases.py
@@ -11,19 +11,17 @@ router = APIRouter(prefix="/diseases", tags=["Diseases"])
 # 전체 질환 목록 조회
 @router.get("", summary="전체 질환 목록 조회", description="전체 질환 목록 조회합니다.")
 def get_all_diseases_name_endpoint(db: Session = Depends(get_db)):
-    diseases = get_all_diseases_name(db)
-    if not diseases:
+    try:
+        diseases = get_all_diseases_name(db)  
         return ResultResponseModel(
-        status_code=400,
-        message="전체 질환 목록 조회 실패",
-        data=None
-    )   
-    return ResultResponseModel(
-        status_code=200,
-        message="전체 질환 목록 조회 성공",
-        data=diseases
-    )
-
+            status_code=200,
+            message="전체 질환 목록 조회 성공",
+            data=diseases
+        )
+    except Exception as e:
+        raise HTTPException(
+            status_code=500,
+            detail="전체 질환 목록 조회 실패")
 
 # 특정 질환 상세 조회
 @router.get("/{disease_id}", summary="질환 상세 조회", description="질환 상세 조회합니다.")
@@ -36,12 +34,10 @@ def get_disease_by_id_endpoint(disease_id: int, db: Session = Depends(get_db)):
             data=disease
         )
     except HTTPException as e:
-        return ResultResponseModel(
+        raise HTTPException(
             status_code=400,
-            message="찾을 수 없는 잘환id 입니다.",
-            data=None)
+            detail="찾을 수 없는 잘환id 입니다.")
     except Exception as e:
-        return ResultResponseModel(
+        raise HTTPException(
             status_code=500,
-            message="등록되지 않은 질환입니다.",
-            data=None)
+            detail="등록되지 않은 질환입니다.")
